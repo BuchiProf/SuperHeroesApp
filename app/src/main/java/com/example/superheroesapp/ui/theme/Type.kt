c96b604996bde2a7f91aff34459dfa997c8cf771
BuchiProf <profbuchi@gmail.comm>
2024-12-14 16:39:05 +0100

    ajout image et polices

--- app/src/main/java/com/example/superheroesapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroesapp/ui/theme/Type.kt
@@ -5,6 +5,18 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.example.superheroesapp.R
+
+val CabinRegular = FontFamily(
+    Font(R.font.cabin_regular)
+)
+
+val CabinBold = FontFamily(
+    Font(R.font.cabin_bold),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
